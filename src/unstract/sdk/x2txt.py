c3d1e2dab09697ca74cdda92f3f523ec97c29ae6
gayathrivijayakumar <gayathri@zipstack.com>
2024-02-20 21:57:17 +0530

    X2Text adapter support added

---
+++ src/unstract/sdk/x2txt.py
@@ -0,0 +1,51 @@
+from abc import ABCMeta
+
+from unstract.adapters.constants import Common
+from unstract.adapters.x2text import adapters
+from unstract.adapters.x2text.constants import X2TextConstants
+from unstract.adapters.x2text.x2text_adapter import X2TextAdapter
+
+from unstract.sdk.adapters import ToolAdapter
+from unstract.sdk.constants import LogLevel
+from unstract.sdk.tool.base import BaseTool
+
+
+class X2Text(metaclass=ABCMeta):
+    def __init__(self, tool: BaseTool):
+        self.tool = tool
+        self.x2text_adapters = adapters
+
+    def get_x2text(self, adapter_instance_id: str) -> X2TextAdapter:
+        try:
+            x2text_config = ToolAdapter.get_adapter_config(
+                self.tool, adapter_instance_id
+            )
+            x2text_adapter_id = x2text_config.get(Common.ADAPTER_ID)
+            if x2text_adapter_id in self.x2text_adapters:
+                x2text_adapter = self.x2text_adapters[x2text_adapter_id][
+                    Common.METADATA
+                ][Common.ADAPTER]
+                x2text_metadata = x2text_config.get(Common.ADAPTER_METADATA)
+                # Add x2text service host, port and platform_service_key
+                x2text_metadata[
+                    X2TextConstants.X2TEXT_HOST
+                ] = self.tool.get_env_or_die(X2TextConstants.X2TEXT_HOST)
+                x2text_metadata[
+                    X2TextConstants.X2TEXT_PORT
+                ] = self.tool.get_env_or_die(X2TextConstants.X2TEXT_PORT)
+                x2text_metadata[
+                    X2TextConstants.PLATFORM_SERVICE_API_KEY
+                ] = self.tool.get_env_or_die(
+                    X2TextConstants.PLATFORM_SERVICE_API_KEY
+                )
+
+                x2text_adapter_class = x2text_adapter(x2text_metadata)
+
+                return x2text_adapter_class
+
+        except Exception as e:
+            self.tool.stream_log(
+                log=f"Unable to get x2text adapter {adapter_instance_id}: {e}",
+                level=LogLevel.ERROR,
+            )
+            return None
---
+++ tests/test_x2text.py
@@ -0,0 +1,60 @@
+import json
+import logging
+import os
+import unittest
+from typing import Any
+
+from dotenv import load_dotenv
+from parameterized import parameterized
+
+from unstract.sdk.tool.base import BaseTool
+from unstract.sdk.x2txt import X2Text
+
+load_dotenv()
+
+logger = logging.getLogger(__name__)
+
+
+def get_test_values(env_key: str) -> list[str]:
+    values = json.loads(os.environ.get(env_key))
+    return values
+
+
+def get_env_value(env_key: str) -> str:
+    value = os.environ.get(env_key)
+    return value
+
+
+class ToolX2TextTest(unittest.TestCase):
+    class MockTool(BaseTool):
+        def run(
+            self,
+            params: dict[str, Any] = {},
+            settings: dict[str, Any] = {},
+            workflow_id: str = "",
+        ) -> None:
+            # self.stream_log("Mock tool running")
+            pass
+
+    @classmethod
+    def setUpClass(cls):
+        cls.tool = cls.MockTool()
+
+    @parameterized.expand(get_test_values("X2TEXT_TEST_VALUES"))
+    def test_get_x2text(self, adapter_instance_id):
+        tool_x2text = X2Text(tool=self.tool)
+        x2text = tool_x2text.get_x2text(adapter_instance_id)
+        self.assertIsNotNone(x2text)
+        self.assertTrue(x2text.test_connection())
+
+        input_file = get_env_value("INPUT_FILE_PATH")
+        output_file = get_env_value("OUTPUT_FILE_PATH")
+        if os.path.isfile(output_file):
+            os.remove(output_file)
+        x2text.process(input_file, output_file)
+        file_size = os.path.getsize(output_file)
+        self.assertGreater(file_size, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
