ad16003487b6927d86bc16c169c8dcea3fdde89b
harini-venkataraman <harini@zipstack.com>
2024-06-10 13:55:15 +0530

    Fixing timeout

--- src/unstract/sdk/prompt.py
+++ src/unstract/sdk/prompt.py
@@ -2,7 +2,7 @@ import logging
 from typing import Any, Optional
 
 import requests
-from requests import ConnectionError, RequestException, Response, Timeout
+from requests import ConnectionError, RequestException, Response
 
 from unstract.sdk.constants import LogLevel, PromptStudioKeys, ToolEnv
 from unstract.sdk.helper import SdkHelper
@@ -75,13 +75,6 @@ class PromptTool:
         except ConnectionError as connect_err:
             msg = "Unable to connect to prompt service. Please contact admin."
             self._stringify_and_stream_err(connect_err, msg)
-            result["error"] = msg
-        except Timeout as time_out:
-            msg = (
-                "Request to run prompt has timed out. "
-                "Probable causes might be connectivity issues in LLMs."
-            )
-            self._stringify_and_stream_err(time_out, msg)
             result["error"] = msg
         except RequestException as e:
             # Extract error information from the response if available
