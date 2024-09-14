181d5f3d70948659806a78bbd40068ca857d1d31
Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>
2024-09-15 01:04:57 +0530

    Pass in httpx client in Azure OpenAI embedding initialisation & increase default timeouts (#101)
    * COnfigure timeputs for httpx clients and increase embedding timeout
    
    * Update src/unstract/sdk/adapters/embedding/azure_open_ai/src/azure_open_ai.py
    
    Co-authored-by: Ritwik G <100672805+ritwik-g@users.noreply.github.com>
    Signed-off-by: Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>
    
    * Add timeout changes to OpenAI
    
    * Add model field for OpenAI
    
    ---------
    
    Signed-off-by: Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>
    Co-authored-by: Ritwik G <100672805+ritwik-g@users.noreply.github.com>

--- src/unstract/sdk/__init__.py
+++ src/unstract/sdk/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.48.2"
+__version__ = "0.49.0"
 
 
 def get_sdk_version():
--- src/unstract/sdk/adapters/embedding/azure_open_ai/src/azure_open_ai.py
+++ src/unstract/sdk/adapters/embedding/azure_open_ai/src/azure_open_ai.py
@@ -1,6 +1,7 @@
 import os
 from typing import Any
 
+import httpx
 from llama_index.core.embeddings import BaseEmbedding
 from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
 
@@ -18,7 +19,7 @@ class Constants:
     DEPLOYMENT_NAME = "deployment_name"
     API_TYPE = "azure"
     TIMEOUT = "timeout"
-    DEFAULT_TIMEOUT = 60
+    DEFAULT_TIMEOUT = 240
 
 
 class AzureOpenAI(EmbeddingAdapter):
@@ -59,6 +60,8 @@ class AzureOpenAI(EmbeddingAdapter):
                 config=self.config
             )
             timeout = int(self.config.get(Constants.TIMEOUT, Constants.DEFAULT_TIMEOUT))
+            httpx_timeout = httpx.Timeout(timeout, connect=60.0)
+            httpx_client = httpx.Client(timeout=httpx_timeout)
             embedding: BaseEmbedding = AzureOpenAIEmbedding(
                 model=str(self.config.get(Constants.MODEL)),
                 deployment_name=str(self.config.get(Constants.DEPLOYMENT_NAME)),
@@ -68,6 +71,7 @@ class AzureOpenAI(EmbeddingAdapter):
                 embed_batch_size=embedding_batch_size,
                 api_type=Constants.API_TYPE,
                 timeout=timeout,
+                http_client=httpx_client,
             )
             return embedding
         except Exception as e:
