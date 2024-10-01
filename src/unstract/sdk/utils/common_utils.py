4b44c02ef9b26f28adc3ad1d3053bea39120f6a6
Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
2024-10-01 12:19:56 +0530

    feat: Changes to measure time taken in index (#106)
    * Changes to measure time taken in index and support for other operations
    
    * Minor docstring fix
    
    * Updated debug log to info for logging timing

--- src/unstract/sdk/utils/common_utils.py
+++ src/unstract/sdk/utils/common_utils.py
@@ -1,4 +1,6 @@
+import functools
 import logging
+import time
 import uuid
 
 from unstract.sdk.constants import LogLevel
@@ -20,3 +22,27 @@ PY_TO_UNSTRACT_LOG_LEVEL = {
     logging.WARNING: LogLevel.WARN,
     logging.ERROR: LogLevel.ERROR,
 }
+
+
+def log_elapsed(operation):
+    """Adds an elapsed time log.
+
+    Args:
+        operation (str): Operation being measured
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                end_time = time.time()
+                elapsed_time = end_time - start_time
+                logger.info(f"Time taken for '{operation}': {elapsed_time:.3f}s")
+            return result
+
+        return wrapper
+
+    return decorator
