a2436cfd6c65e652e3ef6b483dc9bcbb31ac9cfc
harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>
2024-10-17 14:15:17 +0530

    [FEAT] NoOp adapters for load testing (#109)
    * Exception handling for Prompt Service
    
    * Initial commit for No Op Adapters for Load testing
    
    * Update src/unstract/sdk/adapters/embedding/no_op_embedding/src/static/json_schema.json
    
    Co-authored-by: Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
    Signed-off-by: harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>
    
    * Update src/unstract/sdk/adapters/llm/no_op_llm/src/__init__.py
    
    Co-authored-by: Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
    Signed-off-by: harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>
    
    * Update readme text
    
    Co-authored-by: Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
    Signed-off-by: harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>
    
    * Update ReadMe text
    
    * Remove sleep for test connections
    
    * Adding details for usabilitiy
    
    * Changing wait time data type to float
    
    * renaming methods
    
    * Update src/unstract/sdk/llm.py
    
    Co-authored-by: Hari John Kuriakose <hari@zipstack.com>
    Signed-off-by: harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>
    
    * Write extracted file
    
    * Removing check for NoOp from base
    
    * Renames to no-op folders
    
    * Version bump for SDK
    
    * File renames
    
    * Adding sleep for test connection
    
    ---------
    
    Signed-off-by: harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>
    Co-authored-by: Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
    Co-authored-by: Hari John Kuriakose <hari@zipstack.com>

--- src/unstract/sdk/index.py
+++ src/unstract/sdk/index.py
@@ -15,6 +15,9 @@ from llama_index.core.vector_stores import (
 
 from unstract.sdk.adapter import ToolAdapter
 from unstract.sdk.adapters.exceptions import AdapterError
+from unstract.sdk.adapters.vectordb.no_op.src.no_op_custom_vectordb import (
+    NoOpCustomVectorDB,
+)
 from unstract.sdk.adapters.x2text.constants import X2TextConstants
 from unstract.sdk.adapters.x2text.dto import TextExtractionResult
 from unstract.sdk.adapters.x2text.llm_whisperer.src import LLMWhisperer
@@ -290,8 +293,23 @@ class Index:
                 usage_kwargs=usage_kwargs,
                 process_text=process_text,
             )
+
             if not extracted_text:
                 raise IndexingError("No text available to index")
+
+            # For No-op adapters, addition of nodes to vectorDB should not happen
+            # and this has to be handled in the adapter level. But there are a
+            # few challenges considering callback manager and upstream Llama index
+            # method invocations. Hence, making this check here and returning
+            # the doc id to maintain the legacy flow of adapters.
+
+            if isinstance(
+                vector_db.get_vector_db(
+                    adapter_instance_id=vector_db_instance_id, embedding_dimension=1
+                ),
+                (NoOpCustomVectorDB),
+            ):
+                return doc_id
 
             self.index_to_vector_db(
                 vector_db=vector_db,
