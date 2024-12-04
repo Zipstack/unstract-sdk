f1402c0129cb18e713be2d1119e5a6f79f8443ca
Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>
2024-12-04 12:35:46 +0530

    Refactoring for changed file names (#132)
    * Refactoring changed file names
    
    * Roll version
    
    * Update src/unstract/sdk/utils/tool_utils.py
    
    Co-authored-by: Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
    Signed-off-by: Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>
    
    * Update tests/test_fs_permanent.py
    
    Co-authored-by: Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
    Signed-off-by: Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>
    
    * Update tests/test_fs_permanent.py
    
    Co-authored-by: Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
    Signed-off-by: Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>
    
    * Update src/unstract/sdk/utils/tool_utils.py
    
    Co-authored-by: Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
    Signed-off-by: Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>
    
    * Address review comments
    
    ---------
    
    Signed-off-by: Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>
    Co-authored-by: Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>

--- src/unstract/sdk/tool/base.py
+++ src/unstract/sdk/tool/base.py
@@ -14,7 +14,7 @@ from unstract.sdk.constants import (
     ToolEnv,
     ToolExecKey,
 )
-from unstract.sdk.file_storage.fs_shared_temporary import SharedTemporaryFileStorage
+from unstract.sdk.file_storage.shared_temporary import SharedTemporaryFileStorage
 from unstract.sdk.tool.mixin import ToolConfigHelper
 from unstract.sdk.tool.parser import ToolArgsParser
 from unstract.sdk.tool.stream import StreamMixin
@@ -61,7 +61,8 @@ class BaseTool(ABC, StreamMixin):
                     "Please check your settings."
                 )
             self.workflow_filestorage = SharedTemporaryFileStorage(
-                provider=self.filestorage_provider, **self.filestorage_credentials
+                provider=self.filestorage_provider,
+                **self.filestorage_credentials,
             )
 
     @classmethod
