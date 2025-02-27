0238ee0b0244b7aae5c78fe70de3447cd97dbdac
Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
2025-02-27 11:58:41 +0530

    Update src/unstract/sdk/utils/common_utils.py
    Co-authored-by: vishnuszipstack <117254672+vishnuszipstack@users.noreply.github.com>
    Signed-off-by: Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>

--- src/unstract/sdk/utils/common_utils.py
+++ src/unstract/sdk/utils/common_utils.py
@@ -42,7 +42,6 @@ class Utils:
         """
         for unit in ("", "K", "M", "G", "T"):
             if abs(num) < 1024.0:
-                # return f"{num:3.1f} {unit}{suffix}"
                 return f"{num:.2f} {unit}{suffix}"
             num /= 1024.0
         return f"{num:.2f} {suffix}"
